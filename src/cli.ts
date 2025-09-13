d2f66f0414cc5c0157aaf556e13f75ac1da9cee7
Cell <1024@lruihao.cn>
2025-09-13 12:41:20 +0800

    fix(cli): add version check for auto-changelog-plus

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+import { readFileSync } from 'node:fs'
 import { dirname, join } from 'node:path'
 import process from 'node:process'
 import { fileURLToPath } from 'node:url'
@@ -7,9 +8,20 @@ import { run } from 'auto-changelog/src/run.js'
 
 const argv: string[] = process.argv
 const __root: string = dirname(dirname(fileURLToPath(import.meta.url)))
+
+// Check for version flag
+if (argv.includes('-V') || argv.includes('--version')) {
+  const packageJsonPath: string = join(__root, 'package.json')
+  const packageJson = JSON.parse(readFileSync(packageJsonPath, 'utf8'))
+  console.log(packageJson.version)
+  process.exit(0)
+}
+
 const configPath: string = join(__root, 'settings/config.json')
 const templatePath: string = join(__root, 'settings/conventional.hbs')
 const setupPath: string = join(__root, 'settings/setup.cjs')
+
+// Add default config, template and setup if not provided
 if (!argv.includes('--config') && !argv.includes('-c')) {
   argv.push('--config', configPath)
 }
